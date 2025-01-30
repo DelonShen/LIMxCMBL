b4ce789bd558ce376be7ed0eb9095457ef8bef11
Delon Liu Shen <delonshen@gmail.com>
2025-01-29 17:29:27 -0800

    computed SNR for full noise covariance

--- LIMxCMBL/noise.py
+++ LIMxCMBL/noise.py
@@ -1,58 +1,37 @@
 from LIMxCMBL.init import *
+from LIMxCMBL.cross_spectrum import *
 
-from scipy.integrate import quad_vec
+from scipy.integrate import quad_vec, trapezoid
 from tqdm import trange
 
-def get_eHIeHI(Pei, chimin, chimax, dchi, Lambda, elems = False):
-    eHIeHI = np.zeros((len(chibs), len(chibs)), dtype=np.float64)
-    print('computing eIeI')
+def get_eHIeHI(chimin, chimax, Lambda):
+    """
+    No PeI included, have to multiply afterwards
+    """
+    chis_restricted = chis_resample[np.where((chis_resample >= chimin) & (chis_resample <= chimax))]
+    _chis  = chis_restricted.reshape(-1, 1)
+    _chips = chis_restricted.reshape(1 ,-1)
+    dchi = np.mean(np.diff(chis_restricted))
+
+    eHIeHI = np.zeros((len(chis_restricted), len(chis_restricted)), dtype=np.float64)
+#    print('computing eIeI')
     eIeI = np.zeros_like(eHIeHI)
-    for chi_idx in range(len(chibs)):
-        chi = chibs[chi_idx]
-        if(chi > chimin and chi < chimax):
-            eIeI[chi_idx, chi_idx] = 1 / (dchi * chi**2) 
-    eIeI *= Pei
+    for chi_idx in range(len(chis_restricted)):
+        chi = chis_restricted[chi_idx]
+        eIeI[chi_idx, chi_idx] = 1 / (dchi * chi**2) 
 
-    print('computing eIeLO')
+#    print('computing eIeLO')
     eIeLO = np.zeros_like(eHIeHI)
-    for chi_idx in range(len(chibs)):
-        chi = chibs[chi_idx]
-        if(chi > chimin and chi < chimax):
-            for chip_idx in range(len(chibs)):
-                chip = chibs[chip_idx]
-                eIeLO[chi_idx, chip_idx] = 1/chi**2 * Lambda/np.pi * np.sinc(Lambda * (chi - chip))
-    eIeLO *= Pei
+    eIeLO = 1/_chis**2 * Lambda / np.pi * np.sinc(Lambda * (_chis - _chips))
 
-
-    print('computing eLOeI')
+#    print('computing eLOeI')
     eLOeI = np.zeros_like(eHIeHI)
-    for chi_idx in range(len(chibs)):
-        chi = chibs[chi_idx]
-        for chip_idx in range(len(chibs)):
-            chip = chibs[chip_idx]
-            if(chip > chimin and chip < chimax):
-                eLOeI[chi_idx, chip_idx] = 1/chip**2 * Lambda/np.pi * np.sinc(Lambda * (chip - chi))
-    eLOeI *= Pei       
+    eLOeI = 1/_chips**2 * Lambda / np.pi * np.sinc(Lambda * (_chis - _chips))
 
-    print('computing eLOeLO')
+#    print('computing eLOeLO')
+    def integrand(_chib):
+        return Lambda**2 / np.pi**2 / _chib ** 2 * np.sinc(Lambda * (_chis - _chib)) * np.sinc(Lambda * (_chips - _chib))
+    eLOeLO, _ = quad_vec(integrand, chimin, chimax, 
+                      epsabs = 0.0, epsrel = 1e-3)
 
-    def KepsLO(chip, chi):
-        return np.sinc(Lambda * (chi - chip)) * Lambda / np.pi
-    
-    eLOeLO = np.zeros_like(eHIeHI)
-    for chi_idx in trange(len(chibs)):
-        chi = chibs[chi_idx]
-        
-        def integrand(chib):
-            return 1/chib**2 * KepsLO(chib, chi) * KepsLO(chib, chibs)
-            
-        eLOeLO[chi_idx], _ = quad_vec(integrand, 
-                                         chimin, chimax,
-                                         epsabs = 0.0, 
-                                         epsrel = 1e-3)
-    eLOeLO *= Pei
-
-    if(elems):
-        return eIeI, eLOeLO, eIeLO, eLOeI
-
-    return eIeI + eLOeLO - eIeLO - eLOeI
+    return eIeI, eLOeLO, eIeLO, eLOeI
