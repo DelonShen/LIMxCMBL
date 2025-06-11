5bc5f44ab2886dc35b2469cb8e311e44ba9c6dc7
Delon Shen <delonshen@gmail.com>
2025-06-11 11:51:05 -0700

    tweaked toy model figures

--- 010.023-03-28-transfer-to-sherlock.sh
+++ 010.023-03-28-transfer-to-sherlock.sh
@@ -1,8 +1,8 @@
 #dang arg lists being too long to scp regularly...
 
-for f in /sdf/scratch/kipac/delon/I_auto/comb_HI_zmin_1.0_zmax_1.3*; do 
+for f in /sdf/scratch/kipac/delon/I_auto/comb_Lya_zmin_5.2_zmax_8.0*; do 
   echo $f
-  cp $f /sdf/scratch/users/d/delon/CHIME/
+  cp $f /sdf/scratch/kipac/delon/SPHEREx
 done
 
-scp -r /sdf/scratch/users/d/delon/CHIME delon@sherlock:/scratch/users/delon/LIMxCMBL/I_auto/from_s3df/
+scp -r /sdf/scratch/kipac/delon/SPHEREx delon@sherlock:/scratch/users/delon/LIMxCMBL/I_auto/from_s3df/
